9b16cb6fde23a7da71905cd90cddf99602aaea87
ojilon <ojilongkefa@gmail.com>
2026-08-18 20:13:50 +0300

    make the views horizontal, learn concept of 'id' and 'name'

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,28 @@
+const topics = [
+    {
+        id:"groewth",
+        name: "Growth",
+    },
+    {
+        id: "water-relations",
+        name: "Water Relations",
+    },
+    {
+        id:"photosynthesis",
+        name:"Photosynthesis",
+    },
+]
+
 function Sidebar() {
   return (
     <aside>
-      <h2>Projects</h2>
+      <h2>Plant physiology</h2>
+
+      <ul>
+        {topics.map((topic) => (
+            <li key={topic.id}>{topic.name}</li>
+        ))}
+      </ul>
     </aside>
   )
 }
